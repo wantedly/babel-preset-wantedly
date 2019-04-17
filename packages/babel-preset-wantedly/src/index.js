2366837f802e98a919dff9ea51bbaa70b5a138fd
Yuki Yamada <yamadayuuki0818@gmail.com>
2019-04-17 13:56:29 +0900

    Fix

--- packages/babel-preset-wantedly/src/index.js
+++ packages/babel-preset-wantedly/src/index.js
@@ -1,7 +1,9 @@
 import { declare } from "@babel/helper-plugin-utils";
 
-export default declare((api, { jsxPragma, isTSX, allExtensions }) => {
+export default declare((api, options) => {
   api.assertVersion(7);
+
+  const { jsxPragma, isTSX, allExtensions } = options;
 
   return {
     presets: [["@babel/preset-typescript", { jsxPragma, isTSX, allExtensions }]],
